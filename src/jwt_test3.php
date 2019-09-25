4bbb437628a6f09b68f4d3254f0310f2f62d90e5
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-09-25 14:37:58 -0300

    Formatação do terceiro exemplo

--- src/jwt_test3.php
+++ src/jwt_test3.php
@@ -12,12 +12,11 @@
     $time = time();
 
     $jwt_token = (new Builder())
-        ->issuedAt($time)
-        ->canOnlyBeUsedAfter($time + 60)
-        ->expiresAt($time + 3600)
         ->withClaim("user_id", 123)
         ->withClaim("user_name", "Joao Silva")
         ->withClaim("user_email", "teste@gmail.com")
+        ->issuedAt($time)
+        ->expiresAt($time + 3600)
         ->getToken($jwt_signer, new Key($jwt_secret));
 
     echo("<h3>Token</h3><h4>".$jwt_token."</h4>");
@@ -25,4 +24,6 @@
     echo("<br><h3>Token Claims</h3><h4>".json_encode($jwt_token->getClaims())."</h4>");
     echo("<br><h3>Token Verify (Correct key)</h3><h4>".json_encode($jwt_token->verify($jwt_signer, $jwt_secret))."</h4>");
     echo("<br><h3>Token Verify (Wrong key)</h3><h4>".json_encode($jwt_token->verify($jwt_signer, "BatmanAndRobin"))."</h4>");
+
+    echo("<br>Repo-link: <a href='https://github.com/HenriqueMitsuo/PHP_jwt_testing/blob/master/src/jwt_test3.php'>https://github.com/HenriqueMitsuo/PHP_jwt_testing/blob/master/src/jwt_test3.php</a>");
 ?>
